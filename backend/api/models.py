7edf82e9bcf5cae47c74c8b4ff59da8bbacefb8b
JakobHallin <JakobHallin@github.com>
2025-11-13 23:13:37 +0000

    succesed with migrate user to sqlite

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-	id = models.INTEGER(max_lenght = 100, primary_key=True)
+	id = models.IntegerField(primary_key=True)
 	email = models.CharField(max_length=128)
 	username = models.CharField(max_length=128)
 	password = models.CharField(max_length=128)
