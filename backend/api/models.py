0a2e4825b9e88d005f82714cdaf570a619d428d8
JakobHallin <JakobHallin@github.com>
2025-11-13 22:39:59 +0000

    Add user to model.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+	id = models.INTEGER(max_lenght = 100, primary_key=True)
+	email = models.CharField(max_length=128)
+	username = models.CharField(max_length=128)
+	password = models.CharField(max_length=128)
+	
+	def __str__(self):
+		return self.name
